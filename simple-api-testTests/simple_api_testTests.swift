ecfd92e8f11a33d78b0e72aba652e7dcc4bc0d17
esoft16 - Henrique Akiyoshi Eguchi Pires Ribeiro - Grupo 4 <henrique.eguchi@outlook.com>
2019-10-28 19:36:33 -0300

    Initial Commit

---
+++ simple-api-testTests/simple_api_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  simple_api_testTests.swift
+//  simple-api-testTests
+//
+//  Created by Henrique Akiyoshi Eguchi on 28/10/19.
+//  Copyright © 2019 Henrique Akiyoshi Eguchi. All rights reserved.
+//
+
+import XCTest
+@testable import simple_api_test
+
+class simple_api_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
